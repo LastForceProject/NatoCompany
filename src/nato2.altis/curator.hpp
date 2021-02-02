e6feb6713b084df08f8dfb7a9a6fba6f8645726d
FlinVinda <69419361+FlinVinda@users.noreply.github.com>
2021-02-02 17:06:21 +0300

    Update curator.hpp
    Адпейт зевсов

--- src/nato2.altis/curator.hpp
+++ src/nato2.altis/curator.hpp
@@ -6,16 +6,18 @@ class Curators
 		,"76561198288271495"//KapayJl (Tech)
 		,"76561198041211224"//Flin (Tech/curator)
 		,"76561198072246605"//Prosto Igrok SCHTAMP (Pidr)
-        ,"76561198054599597"//Olaf (zam_curator)
+     	        ,"76561198054599597"//Olaf (zam_curator)
+		,"76561198121927537"//Kredo(zam_curator)
 		,"76561199028516754"//Desire (tech)
 		,"76561198384407714"//Crow (Event)
-        ,"76561198044799547"//Glyba (Event)
+                ,"76561198044799547"//Glyba (Event)
 		,"76561198295847201"//white (Event)
 		,"76561198053678180"//Leo(Event)
 		,"76561198391711601"//grif(Event)
 		,"76561198035250386"//Dias (main zeus)
 		,"76561198024177091"//Lem(Event)
                 ,"76561198209489483"//PO(Event)
-		,"76561198121927537"//Kredo(zam_curator)
+                ,"76561199020692739"//Blair(Event)
+		
 };
 };
